e8c5d3a5c13e4bdc9a91360426036ff6e3549015
km070105 <k.marcojos.142310.tc@umindanao.edu.ph>
2026-03-06 02:04:54 +0800

    fix: resolve all merge conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-<<<<<<< HEAD
 )
-=======
-)
->>>>>>> 459101b72b340cb7ffeaad24eb24ffb218f67344
